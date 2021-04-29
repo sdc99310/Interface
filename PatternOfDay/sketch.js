a96aa45d8fcf6a889ad21212cf1ccc5daa53df37
sdc99310 <sdc99310@gmail.com>
2021-04-29 18:45:37 -0400

    Update sketch.js

--- PatternOfDay/sketch.js
+++ PatternOfDay/sketch.js
@@ -4,12 +4,26 @@ function setup() {
 }
 
 function draw(){
+
+  translate(width/2, height/2); //trasnlate x,y to the center
+
+  let mx = mouseX - width/2;
+  let my = mouseY - height/2;
+  let pmx = pmouseX - width/2;
+  let pmy = pmouseY - height/2; //mouse position relative to the center
+
   if (mouseIsPressed) {
-    stroke(255);
+    stroke(0);
+
+    for (let i=0; i<10; i++) { //repeat 6 times
 
     let d = dist(mouseX, mouseY, pmouseX, mouseY);
-  }
+    let sw = map(d, 0, 20, 20, 1); //<1 is strokeWeight
+    strokeWeight(sw);
+    line(mouseX, mouseY, pmouseX, pmouseY);
 
+    }
+  }
 
 
 }
