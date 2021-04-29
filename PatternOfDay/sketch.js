f7e3848322babeb9978d2049ff9c8e9707f63c3b
sdc99310 <sdc99310@gmail.com>
2021-04-29 18:58:40 -0400

    .

--- PatternOfDay/sketch.js
+++ PatternOfDay/sketch.js
@@ -1,6 +1,19 @@
+let symmetry = 12;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
   background(350);
+  translate(width/2, height/2);
+  stroke(0, 0, 0);
+
+  let angle = 360/ symmetry;
+    for (let i=0; i<symmetry; i++) { //repeat drawing 10 times
+      rotate(angle);
+      line(0, 0,width, 0);
+    }
+
 }
 
 function draw(){
@@ -13,14 +26,16 @@ function draw(){
   let pmy = pmouseY - height/2; //mouse position relative to the center
 
   if (mouseIsPressed) {
-    stroke(0);
+    stroke(0, 100);
 
-    for (let i=0; i<10; i++) { //repeat 6 times
+    let angle = 360/12;
+    for (let i=0; i<12; i++) { //repeat drawing 10 times
+      rotate(i*60)
 
-    let d = dist(mouseX, mouseY, pmouseX, mouseY);
-    let sw = map(d, 0, 20, 20, 1); //<1 is strokeWeight
+    let d = dist(mx, my, pmx, pmy);
+    let sw = map(d, 0, 20, 20, 10); //<1 is strokeWeight
     strokeWeight(sw);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(mx, my, pmx, pmy);
 
     }
   }
