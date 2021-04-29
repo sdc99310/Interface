35b9509a5906d2808b818132d6dddcb093ac6fef
sdc99310 <sdc99310@gmail.com>
2021-04-29 14:35:11 -0400

    update

---
+++ PatternOfDay/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(350);
+}
+
+function draw(){
+  if (mouseIsPressed) {
+    stroke(255);
+
+    let d = dist(mouseX, mouseY, pmouseX, mouseY);
+  }
+
+
+
+}
+
+  
